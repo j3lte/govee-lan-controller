fb0b06ab1fff68bc758cd5835c27bc476b3e34da
j3lte <jwlagendijk@gmail.com>
2023-11-15 00:31:34 +0100

    Export types

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { colord, fade, random, getRandomColor } from "./lib/color";
 export type { Colord } from "./lib/color";
 export * as actions from "./actions";
+export * from "./types";
 export { Govee } from "./govee";
 export { Device } from "./device";
 
