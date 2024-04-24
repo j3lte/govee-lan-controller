b73519c6250ff27107af405b65296e9bd3d9162e
j3lte <jwlagendijk@gmail.com>
2024-04-24 12:50:23 +0200

    Update exports

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import { colord, fade, random, getRandomColor } from "./lib/color";
-export type { Colord } from "./lib/color";
+export type { Colord, ColorInput } from "./lib/color";
 export * as actions from "./actions";
 export * from "./types";
 export { Govee } from "./govee";
+export type { GoveeOptions } from "./govee";
 export { Device, DeviceState } from "./device";
 
 export const color = {
